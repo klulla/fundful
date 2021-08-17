c2e1bd06560b30902252b1b64b781de4e83dbfd8
klulla <43425693+klulla@users.noreply.github.com>
2021-08-16 22:55:53 -0400

    New Card Stuff

--- src/comps/HomeScreen.js
+++ src/comps/HomeScreen.js
@@ -1,54 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import Example from './NavBar'
+import Card from './Card'
+import { useSpring, animated } from 'react-spring';
+import styled from 'styled-components';
+import { MdClose } from 'react-icons/md';
 
 const HomeScreen = () => {
     return (
         <div>
             <Example />
             <div class="flex justify-evenly mt-8">
-            <div class="max-w-xs rounded overflow-hidden shadow-lg my-2 flex-3">
-  <img class="w-full max-h-52" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-grey-darker text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 py-4">
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">Contact at <span class="inline-block bg-grey-lighter rounded-full py-1 text-sm font-bold underline text-grey-darker mr-2">abc123@gmail.com</span></span>
-  </div>
-</div>
-
-<div class="max-w-xs rounded overflow-hidden shadow-lg my-2 flex-3">
-  <img class="w-full max-h-52" src="https://images.unsplash.com/photo-1613047880926-105f6e0662ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80" alt="Sunset in the mountains"></img>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-grey-darker text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 py-4">
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
-  </div>
-</div>
-
-<div class="max-w-xs rounded overflow-hidden shadow-lg my-2 flex-3">
-  <img class="w-full max-h-52" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-grey-darker text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 py-4">
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
-  </div>
-</div>
+                <input class="w-1/2 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter Location"></input>
+                <select placeholder="Category" class="w-1/5 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                    <option value="">Select a Category</option>
+                    <option value="other">Other</option>
+                    <option value="arts">Arts</option>
+                    <option value="automotive">Automotive</option>
+                    <option value="entertainment">Entertainment</option>
+                    <option value="fitness">Fitness</option>
+                    <option value="grocerystore">Grocery Store</option>
+                    <option value="hospitality">Hospitality</option>
+                    <option value="realestate">Real Estate</option>
+                    <option value="restaurant">Restaurant</option>
+                    <option value="technology">Technology</option>
+                </select>
+                <button class="w-20 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Search</button>
             </div>
+            <div class="md:flex md:flex-row md:justify-evenly mt-8 xs:flex xs:flex-col xs:w-full xs:justify-center">
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        
             
         </div>
     )
