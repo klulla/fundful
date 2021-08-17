a45afe4b995a6b85c3fa0f593d93a85ef011d7dc
klulla <43425693+klulla@users.noreply.github.com>
2021-08-16 22:59:40 -0400

    New Cards

---
+++ src/comps/Card.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <div class="max-w-xs rounded overflow-hidden shadow-lg my-2 mb-8">
+  <img class="w-full max-h-52" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
+  <div class="px-6 py-4">
+    <div class="font-bold text-lg mb-2">$250,000 for 10%</div>
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-grey-darker text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+  </div>
+</div>
+
+<div class="max-w-xs rounded overflow-hidden shadow-lg my-2 xs:xs:mb-8">
+  <img class="w-full max-h-52" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
+  <div class="px-6 py-4">
+  <div class="font-bold text-lg mb-2">$250,000 for 10%</div>
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-grey-darker text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+  </div>
+</div>
+
+<div class="max-w-xs rounded overflow-hidden shadow-lg my-2 mt-8">
+  <img class="w-full max-h-52" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"></img>
+  <div class="px-6 py-4">
+  <div class="font-bold text-lg mb-2">$250,000 for 10%</div>
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-grey-darker text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
+    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+  </div>
+</div>
+
+
+        </div>
+    )
+}
+
+export default Card
